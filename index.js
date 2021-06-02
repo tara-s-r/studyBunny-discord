b9cecd51ce038245f238c3561793406a036ddfd0
tara-s-r <44852921+tara-s-r@users.noreply.github.com>
2021-06-01 23:14:20 -0500

    daily progress (6/1)

--- index.js
+++ index.js
@@ -4,26 +4,32 @@ const client = new Discord.Client()
 
 // to let me know the bot has booted up
 client.once("ready", () => {
-  console.log(`${client.user.tag} has logged in!`)
+	console.log(`${client.user.tag} has logged in!`)
 })
 
+// ^sesh 60/5/2 studying
+
+// task 1: set up a study session (study time/break time/# of sessions),
+// pings you each session end and start
 client.on("message", msg => {
-  // check if a message has been sent by a user starting with '^' and remove it to process easier
-  if (!msg.author.bot && msg.content.charAt(0) == '^') {
-    msg.content = msg.content.substring(1)
-    // task 1: set up a study session (study time/break time/# of sessions),
-    // pings you each session end and start
-    if (msg.content.startsWith("sesh")) {
-      msg.channel.send("the createdTimestamp function: " + msg.createdTimestamp)
-      // every 60,000 units, a minute passes
-      // now create a function that throttles when to send a message (outside of this if statement)
-      // and have it send the message that the time is up and ping the person
-      // do it after 1 minute to test and then have it be custom based on the user
-      // also try and see if you can change it to be after a few seconds
-    } else {
-      msg.channel.send("oops!")
-    }
-  }
+	// check if a message has been sent by a user starting with '^' and remove it to process easier
+	if (!msg.author.bot && msg.content.charAt(0) == '^') {
+		msg.content = msg.content.substring(1)
+		if (msg.content.startsWith("sesh")) {
+			// remove "sesh " from the message
+			msg.content = msg.content.substring(5)
+			// find function to parse strings based on spaces (like the hasNext Scanner function)
+			// get the group of numbers, basically anything before the space
+			
+			// what if the user puts a space in between the numbers themselves?
+			// maybe have the bot check the format and give an error if it doesn't fit
+			let delay = 6
+			setTimeout(() => msg.channel.send("session ended!"), 1 * 60 * 1000)
+			// change to custom based on the user
+		} else {
+			msg.channel.send("oops!")
+		}
+	}
 })
 
 client.login(token_file.token)
