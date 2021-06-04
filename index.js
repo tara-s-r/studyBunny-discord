5502338e28a5e1752f7c168c4a5fb3189e52bfcf
tara-s-r <44852921+tara-s-r@users.noreply.github.com>
2021-06-03 21:50:22 -0500

    daily progress (6/3)

--- index.js
+++ index.js
@@ -7,29 +7,60 @@ client.once("ready", () => {
 	console.log(`${client.user.tag} has logged in!`)
 })
 
-// ^sesh 60/5/2 studying
+// ^sesh studying
 
 // task 1: set up a study session (study time/break time/# of sessions),
 // pings you each session end and start
 client.on("message", msg => {
-	// check if a message has been sent by a user starting with '^' and remove it to process easier
+	// check if a message has been sent by a user starting with '^'
 	if (!msg.author.bot && msg.content.charAt(0) == '^') {
-		msg.content = msg.content.substring(1)
-		if (msg.content.startsWith("sesh")) {
-			// remove "sesh " from the message
-			msg.content = msg.content.substring(5)
-			// find function to parse strings based on spaces (like the hasNext Scanner function)
-			// get the group of numbers, basically anything before the space
-			
-			// what if the user puts a space in between the numbers themselves?
-			// maybe have the bot check the format and give an error if it doesn't fit
-			let delay = 6
-			setTimeout(() => msg.channel.send("session ended!"), 1 * 60 * 1000)
-			// change to custom based on the user
+		if (msg.content.substring(1, 5) === "sesh") {
+			// maybe try to do a reaction-type input, where studying/break time is given in 5s
+			// and you can click the next page to get a longer study time
+			// if they take too long to input, give an "operation expired" message & reset
+			const headerLen = 6
+			// get the topic of the session
+			topic = msg.content.substring(headerLen)
+			// do the reactions and stuff
+			msg.channel.send(`This is just a check, is <@${client.user.id}> able to work as a ping?`)
+			reactions(intro(msg.channel), msg.channel)
+			// runSessions();
 		} else {
 			msg.channel.send("oops!")
 		}
 	}
 })
 
+let intro = () => {
+	// split up the text to make it more readable here
+	const intro_1 = "How long is each session? Select the reaction to that number divided by 5, "
+	const intro_2 = "for example if each session is 50 minutes, react to the `10` emoji on the next message."
+	return intro_1 + intro_2
+}
+
+let reactions = async (botMes, chan) => {
+	try {
+        let mes = await chan.send(botMes)
+		await mes.react("1️⃣")
+		await mes.react("2️⃣")
+		await mes.react("3️⃣")
+		await mes.react("4️⃣")
+		await mes.react("5️⃣")
+		await mes.react("6️⃣")
+		await mes.react("7️⃣")
+		await mes.react("8️⃣")
+		await mes.react("9️⃣")
+		await mes.react("🔟")
+		// await mes.react("▶️")
+		// consider making an array of these and using a loop along with >/< keys for pages
+	} catch(error) {
+		console.log(error)
+	}
+}
+
 client.login(token_file.token)
+// chan.send(intro_1 + intro_2).then(botIntro => {
+// 	botIntro.react('1️⃣')
+// 	botIntro.react('🌟')
+// 	.catch(error => console.error('hmm, that didn\'t work the way we wanted', error));
+// })
