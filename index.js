875d5828b7d83ba0a8df2bbea4a38bf24dd7c4f8
tara-s-r <44852921+tara-s-r@users.noreply.github.com>
2021-06-16 09:11:35 -0500

    got the reaction collector to give a(n incorrect) response

--- index.js
+++ index.js
@@ -4,63 +4,66 @@ const client = new Discord.Client()
 
 // to let me know the bot has booted up
 client.once("ready", () => {
-	console.log(`${client.user.tag} has logged in!`)
+	console.log(`${client.user.tag} has entered the chatroom!`)
 })
 
-// ^sesh studying
+// ^sesh
 
-// task 1: set up a study session (study time/break time/# of sessions),
 // pings you each session end and start
 client.on("message", msg => {
 	// check if a message has been sent by a user starting with '^'
 	if (!msg.author.bot && msg.content.charAt(0) == '^') {
 		if (msg.content.substring(1, 5) === "sesh") {
-			// maybe try to do a reaction-type input, where studying/break time is given in 5s
-			// and you can click the next page to get a longer study time
-			// if they take too long to input, give an "operation expired" message & reset
-			const headerLen = 6
-			// get the topic of the session
-			topic = msg.content.substring(headerLen)
 			// do the reactions and stuff
-			msg.channel.send(`This is just a check, is <@${client.user.id}> able to work as a ping?`)
-			reactions(intro(msg.channel), msg.channel)
+			addReactions(msg.channel).then(() => getReaction(msg))
 			// runSessions();
 		} else {
-			msg.channel.send("oops!")
+			msg.channel.send("That doesn't seem to be a command for me.")
 		}
 	}
 })
 
-let intro = () => {
-	// split up the text to make it more readable here
-	const intro_1 = "How long is each session? Select the reaction to that number divided by 5, "
-	const intro_2 = "for example if each session is 50 minutes, react to the `10` emoji on the next message."
-	return intro_1 + intro_2
-}
-
-let reactions = async (botMes, chan) => {
+// sends a message telling the user about how studyBunny's sessions work
+// and adds reactions to that message
+const addReactions = async (botChannel) => {
+	// I split up the text to make it more readable here
+	let botMessage = "How long is each session? Select the reaction to that number divided by 5,"
+					 + " for example if each session is 50 minutes, react to the `10` emoji on the"
+					 + " next message."
 	try {
-        let mes = await chan.send(botMes)
-		await mes.react("1️⃣")
-		await mes.react("2️⃣")
-		await mes.react("3️⃣")
-		await mes.react("4️⃣")
-		await mes.react("5️⃣")
-		await mes.react("6️⃣")
-		await mes.react("7️⃣")
-		await mes.react("8️⃣")
-		await mes.react("9️⃣")
-		await mes.react("🔟")
-		// await mes.react("▶️")
+        let initial = await botChannel.send(botMessage)
+		await initial.react("1️⃣")
+		await initial.react("2️⃣")
+		await initial.react("3️⃣")
+		await initial.react("4️⃣")
+		await initial.react("5️⃣")
+		await initial.react("6️⃣")
+		await initial.react("7️⃣")
+		await initial.react("8️⃣")
+		await initial.react("9️⃣")
+		await initial.react("🔟")
+		return initial
 		// consider making an array of these and using a loop along with >/< keys for pages
+		// await mes.react("▶️")
 	} catch(error) {
 		console.log(error)
 	}
 }
 
+// still needs work
+function getReaction(message){
+	const filter = (reaction, user) => {
+		return ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'].includes(reaction.emoji.name) && user.id === message.author.id;
+	};
+	
+	message.awaitReactions(filter, { max: 1, time: 10000, errors: ['time'] })
+		.then(collected => {
+			const reaction = collected.first();
+			message.reply(`you reacted with a ${reaction}`);
+		})
+		.catch(collected => {
+			message.reply('you reacted with nothing?');
+		});
+}
+
 client.login(token_file.token)
-// chan.send(intro_1 + intro_2).then(botIntro => {
-// 	botIntro.react('1️⃣')
-// 	botIntro.react('🌟')
-// 	.catch(error => console.error('hmm, that didn\'t work the way we wanted', error));
-// })
